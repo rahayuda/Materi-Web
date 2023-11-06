4b5ac2a695fbd0eb33bc09a06915810886378036
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-11-06 14:20:35 +0800

    Add files via upload

--- P8-PHP-GetPost/post.php
+++ P8-PHP-GetPost/post.php
@@ -2,5 +2,18 @@
 	//$name=$_POST["name"];
 	$name=$_REQUEST["name"];
 	$password=$_POST["password"];
-	echo "Welcome: $name, your password is: $password";  
+	echo "Welcome: $name, your password is: $password <br><br>"; 
+
+	$hashedpassword = password_hash($password, PASSWORD_BCRYPT); 
+	echo "1. Enkripsi Bcrypt: $hashedpassword<br>";
+
+	$hashedpassword = hash("sha256", $password);
+	echo "2. Enkripsi SHA256: $hashedpassword<br>";
+
+	$hashedpassword = md5($password);
+	echo "3. Enkripsi MD5: $hashedpassword<br>";
+
+	$key = "kuncipassword";
+	$hashedpassword = hash_hmac("sha256", $password, $key);
+	echo "4. Enkripsi HMAC: $hashedpassword<br>";
 ?>  
