03b5e662ae927173694ae17661cb6014d2978492
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-12-10 16:14:27 +0800

    Update sql.php

--- P13-PHP-SQL-Update/sql.php
+++ P13-PHP-SQL-Update/sql.php
@@ -5,11 +5,6 @@
 	$password	= ""; 
 	$database	= "nim_nama"; 
 
-	//$server		= "sql210.infinityfree.com"; 
-	//$user		= "epiz_33724693";
-	//$password	= "OLd96buhHp8V"; 
-	//$database	= "epiz_33724693_nim_nama"; 
-
 	$con = mysqli_connect($server,$user,$password,$database);
 
 ?>
